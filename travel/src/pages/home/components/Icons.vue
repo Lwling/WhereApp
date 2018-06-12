e657c5f8feb828c7736cbee6f3020fadb7ccf5e6
Lwling <x071@qq.com>
2018-06-12 19:25:06 +0800

    Revert "icons"
    This reverts commit 643a46fef2ce0f348f191464bdafe047bd75ae17.

--- travel/src/pages/home/components/Icons.vue
+++ travel/src/pages/home/components/Icons.vue
@@ -1,83 +1,65 @@
 <template>
     <div class="icons">
-        <swiper>
-        <swiper-slide v-for="(page,index) of pages" :key="index">
-            <div 
-            class="icon"
-             v-for="item of page"
-             :key="item.id"
-             >
-                <div class="icon-img">
-                <img class="icon-img-content" :src="item.imgUrl"/>
-                </div>
-                <p class="icon-desc">{{item.desc}}</p>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
             </div>
-        </swiper-slide>
-        </swiper>
-       
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
     
     </div>
 </template>
 <script>
 export default {
-    name:'HomeIcons',
-data(){
-    return{
-        iconList:[{
-            id:'0001',
-            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-            desc:'景点门票'
-        },{
-            id:'0002',
-            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
-            desc:'踏青赏花'
-        },{
-            id:'0003',
-            imgUrl:'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
-            desc:'必游榜单'
-        },{
-            id:'0004',
-            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
-            desc:'自然风光'
-        },{
-            id:'0005',
-            imgUrl:'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
-            desc:'一日游'
-        },{
-            id:'0006',
-            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
-            desc:'动植物园'
-        },{
-            id:'0007',
-            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
-            desc:'主题乐园'
-        },{
-            id:'0008',
-            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/b8/c5dcdb58deec2402.png',
-            desc:'自然风光'
-        }]
-    }
- },
- computed:{
-     pages(){
-         const pages = []
-         this.iconList.forEach((item,index) => {
-             const page = Math.floor(index / 8)
-             if(!pages[page]){
-                 pages[page] = []
-             }
-             pages[page].push(item)
-         })
-         return pages;
-     }
- }
+    name:'HomeIcons'
 }
 </script>
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
-@import '~styles/mixins.styl'
-.icons >>>.swiper-container
-{
+.icons{
+    height:0
     padding-bottom:50%
     overflow:hidden
     .icon{
@@ -111,9 +93,6 @@ data(){
         line-height :.44rem
         color:$darkTextColor
         text-align:center
-        overflow:hidden
-        white-space:nowrap
-        ellipsis()
     }
 }
 
