507c9c0c629ca0a11ae8b96dd7c3fd7a6425ee2f
Lwling <x071@qq.com>
2018-06-12 16:14:51 +0800

    icons

---
+++ travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="景点门票">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+    
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeIcons'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.icons{
+    height:0
+    padding-bottom:50%
+    overflow:hidden
+    .icon{
+        overflow:hidden
+        float:left
+        width:25%
+        padding-bottom:25%
+        position:relative
+        height:0
+    }
+    .icon-img{
+        position absolute
+        top:0
+        left:0
+        right:0
+        bottom:.44rem
+        box-sizing:border-box
+        padding:.1rem
+        .icon-img-content{
+            display:block
+            margin:0 auto
+            height 100%
+        }
+    }
+    .icon-desc{
+        position:absolute
+        left:0
+        right :0
+        bottom:0
+        height :.44rem
+        line-height :.44rem
+        color:$darkTextColor
+        text-align:center
+    }
+}
+
+</style>
+
+
